1af95cad134117cab8abcd0c51e8f2588d15298a
giuliobosco <giuliobva@gmail.com>
2019-01-06 12:12:40 +0100

    fix Cable - attributes

--- src/cableway/Cable.java
+++ src/cableway/Cable.java
@@ -32,4 +32,134 @@ package cableway;
  */
 public class Cable {
 
+    // ------------------------------------------------------------------------------------ Costants
+    // ---------------------------------------------------------------------------------- Attributes
+
+    /**
+     * Length of the cable.
+     */
+    private double lenght;
+
+    /**
+     * Position of the cable, all methods reported this attribute are synchronized, for keep a
+     * coherent value.
+     */
+    private double position;
+
+    /**
+     * Speed of the cable.
+     */
+    private double speed;
+
+    // --------------------------------------------------------------------------- Getters & Setters
+
+    /**
+     * Set the length of the cable.
+     *
+     * @param length Length of the cable.
+     */
+    private void setLength(double length) {
+        if (length > 0) {
+            this.lenght = length;
+        }
+    }
+
+    /**
+     * Get the length of the cable.
+     *
+     * @return Length of the cable.
+     */
+    public double getLength() {
+        return this.lenght;
+    }
+
+    /**
+     * Set the position of the cable, synchronized me.
+     *
+     * @param position Position of the cable.
+     */
+    public void setPosition(double position) {
+        if (position >= 0 && position <= this.lenght) {
+            this.position = position;
+        }
+    }
+
+    /**
+     * Get the position of the cable.
+     *
+     * @return Position of the cable.
+     */
+    public double getPosition() {
+        return this.position;
+    }
+
+    /**
+     * Set the speed of the cable.
+     *
+     * @param speed Speed of the cable.
+     */
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    /**
+     * Get the speed of the cable.
+     *
+     * @return Speed of the cable.
+     */
+    public double getSpeed() {
+        return this.speed;
+    }
+
+    // -------------------------------------------------------------------------------- Constructors
+
+    /**
+     * Create the cable, with the length of the cable.
+     *
+     * @param length Length of the cable.
+     */
+    public Cable(double length) {
+        this.setLength(length);
+    }
+
+    // -------------------------------------------------------------------------------- Help Methods
+
+    /**
+     * Increment the position of the cable.
+     *
+     *  @param x Value to increment.
+     */
+    public void incrementPosition(double x) {
+        this.position += x;
+    }
+
+    /**
+     * Decrement the position of the cable.
+     *
+     * @param x Value to decrement.
+     */
+    public void decrementPosition(double x) {
+        this.position -= x;
+    }
+
+    /**
+     * Increment the speed of the cable.
+     *
+     * @param x Value to increment.
+     */
+    public void incrementSpeed(double x) {
+        this.speed += x;
+    }
+
+    /**
+     * Decrement the speed of the cable.
+     *
+     * @param x Value to decrement.
+     */
+    public void decrementSpeed(double x) {
+        this.speed -= x;
+    }
+
+    // ----------------------------------------------------------------------------- General Methods
+    // --------------------------------------------------------------------------- Static Components
 }
