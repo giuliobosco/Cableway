cb65a9f518bb94669f296664465aeaa54086a5d1
giuliobosco <giuliobva@gmail.com>
2019-01-08 12:32:48 +0100

    add PeopleSet (with FullSetException)

---
+++ src/cableway/people/PeopleSet.java
@@ -0,0 +1,106 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 giuliobosco.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+ 
+package cableway.people;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * People set.
+ *
+ * @author giuliobosco
+ * @version 1.0
+ */
+public class PeopleSet {
+    // ------------------------------------------------------------------------------------ Costants
+    // ---------------------------------------------------------------------------------- Attributes
+
+    /**
+     * People of the people set.
+     */
+    private List<Person> people;
+
+    /**
+     * Maximum people in the people set.
+     */
+    private int maxPeople;
+
+    // --------------------------------------------------------------------------- Getters & Setters
+    // -------------------------------------------------------------------------------- Constructors
+
+    /**
+     * Create the people set with the maximum people in the people set and initialize the people
+     * list.
+     *
+     * @param maxPeople Maximum people in the people set.
+     * @throws FullSetException Not enough people for the maximum people set.
+     */
+    public PeopleSet(int maxPeople) throws FullSetException {
+        if (maxPeople > 0) {
+            this.maxPeople = maxPeople;
+            this.people = new ArrayList<>();
+        } else {
+            throw new FullSetException("Not enough people for maximum people.");
+        }
+    }
+
+    // -------------------------------------------------------------------------------- Help Methods
+
+    /**
+     * Add person to the people set.
+     *
+     * @param person Person to add.
+     * @throws FullSetException No more people can be added to the people set.
+     */
+    public void addPerson(Person person) throws FullSetException {
+        if (this.people.size() < this.maxPeople) {
+            this.people.add(person);
+        } else {
+            throw new FullSetException("No more people can be added to the people set.");
+        }
+    }
+
+    /**
+     * Remove person from the people set.
+     *
+     * @param person Person to remove
+     */
+    public void removePerson(Person person) {
+        this.people.remove(person);
+    }
+
+    /**
+     * Get the people set is full.
+     *
+     * @return People set is full, true if the people set is full else false.
+     */
+    public boolean isFull() {
+        return this.people.size()==this.maxPeople;
+    }
+
+    // ----------------------------------------------------------------------------- General Methods
+    // --------------------------------------------------------------------------- Static Components
+    
+}
